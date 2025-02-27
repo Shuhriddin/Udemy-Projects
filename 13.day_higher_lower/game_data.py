1948f633225725fd3216f8a15eb3699cf3b07682
Shuhriddin Solixov <152378280+Shuhriddin@users.noreply.github.com>
2025-02-28 00:37:22 +0500

    Projects

---
+++ 13.day_higher_lower/game_data.py
@@ -0,0 +1,44 @@
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 346,
+        'desctiption': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 215,
+        'desctiption': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Ariana Grande',
+        'follower_count': 183,
+        'desctiption': 'Musican and  actreess',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dwayne Jhonson',
+        'follower_count': 181,
+        'desctiption': 'Actor and professional wresler',
+        'country': 'United States'
+    },
+    {
+        'name': 'Selena Gomes',
+        'follower_count': 174,
+        'desctiption': 'Musican and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'follower_count': 172,
+        'desctiption': 'Reality TV personality and businesswoman and Self-Made Bills',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'follower_count': 167,
+        'desctiption': 'Reality TV personality and businessman',
+        'country': 'United States'
+    }
+]
